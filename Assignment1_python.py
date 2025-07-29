c16954e33544ce3560987198f02d5f69879f7469
giftmakhu <58071477+giftmakhu@users.noreply.github.com>
2025-07-29 23:33:16 +0200

    My assignments
    Assignment one

---
+++ Assignment1_python.py
@@ -0,0 +1,30 @@
+# Gift Makhuvele Assignment 1
+# My firstsimple calculator
+
+# Enter the first number
+number1 = float(input("Enter your first number: "))
+
+
+# Enter the  second number️
+number2 = float(input("Enter your second number:\n "))
+
+# Make the calculations
+
+# Addition
+addition_results = number1 + number2
+
+# Subtraction 
+subtraction_result = number1 - number2
+
+# Multiplication
+multiplication_result = number1 * number2
+
+#Division
+division_result = number1 / number2
+
+# Results
+print(f"Results of your two numbers:\n")
+print(f"Sum of the two numbers: {addition_results}\n")  
+print(f"Difference of the two numbers: {subtraction_result}\n")  
+print(f"Product of the tow numbers: {multiplication_result}\n")
+print(f"Division of the two numbers: {division_result}")  
